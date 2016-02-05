1e3f00b00ca3ac19ecf008a87beca32fb62bceee
tangentstorm <michal.wallace@gmail.com>
2016-02-04 21:08:38 -0600

    Dynamically render a crude gallery from json http results.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,21 @@
+import 'rxjs/add/operator/map';
 import {Component} from 'angular2/core';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {Inject} from "angular2/core";
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>svg-gallery!</h1>'
+  selector: 'my-app',
+  providers: [HTTP_PROVIDERS],
+  templateUrl: 'app/templates/gallery.html'
 })
-export class AppComponent { }
-
+export class AppComponent {
+  categories = [];
+  constructor(private http: Http) {
+    http.get('gallery.json')
+      .map(res => res.json())
+      .subscribe(
+        data => this.categories = data['categories'],
+        error => console.error(error)
+      );
+  }
+}
